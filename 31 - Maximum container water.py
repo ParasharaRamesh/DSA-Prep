430fcc0ab17d6a10fd43df00275fb4f8918c2c23
Parashara <parasharar@gmail.com>
2023-04-08 11:12:16 +0530

    31 - Maximum container water

---
+++ 31 - Maximum container water.py
@@ -0,0 +1,79 @@
+from typing import *
+
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        left = 0
+        right = len(heights) - 1
+
+        greatestArea = 0
+
+        while left < right:
+            width = right - left
+
+            if heights[left] < heights[right]:
+                # get curr area
+                height = heights[left]
+                currArea = height * width
+
+                # set new area
+                greatestArea = max(currArea, greatestArea)
+
+                # move left ptr rightward
+                left = self.moveLeftPtr(greatestArea, heights, left, right)
+            else:
+                # get curr area
+                height = heights[right]
+                currArea = height * width
+
+                # set new area
+                greatestArea = max(currArea, greatestArea)
+
+                # move right ptr leftward
+                right = self.moveRightPtr(greatestArea, heights, left, right)
+
+        return greatestArea
+
+    def moveLeftPtr(self, greatestArea, heights, left, right):
+        '''
+        Move left ptr rightward until heights[left] >= heights[right]
+        '''
+        i = left + 1
+        while i < right:
+            width = right - i
+            height = heights[i]
+            currArea = width * height
+
+            if currArea > greatestArea:
+                break
+            i += 1
+
+        return i
+
+    def moveRightPtr(self, greatestArea, heights, left, right):
+        '''
+        Move right ptr leftward until heights[right] >= heights[left]
+        '''
+        i = right - 1
+        while i > left:
+            width = i - left
+            height = heights[i]
+            currArea = width * height
+
+            if currArea > greatestArea:
+                break
+            i -= 1
+
+        return i
+
+
+if __name__ == '__main__':
+    s = Solution()
+    h1 = [1, 8, 6, 2, 5, 4, 8, 3, 7]  # 49
+    print(s.maxArea(h1))
+
+    h2 = [1, 1]  # 1
+    print(s.maxArea(h2))
+
+    h3 = [1, 2, 4, 3]  # 4
+    print(s.maxArea(h3))
