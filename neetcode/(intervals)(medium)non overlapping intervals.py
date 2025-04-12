be066b4070a405f320870611899d1ecaffc2ceeb
Parashara <parasharar@gmail.com>
2025-04-12 18:41:01 +0530

    non overlapping intervals

--- neetcode/(intervals)(medium)non overlapping intervals.py
+++ neetcode/(intervals)(medium)non overlapping intervals.py
@@ -20,11 +20,56 @@ Constraints:
 1 <= intervals.length <= 1000
 intervals[i].length == 2
 -50000 <= starti < endi <= 50000
+
+Insights:
+* sort
+* keep track of only the recent most interval
+* when checking curr interval with recent interval, check if they are overlapping:
+    - if so count ++
+    - keep the interval which has a lower finish, and throw away the other interval which finishes later. This ensures that we dont count more overlaps in the future.
+
 '''
 
 from typing import List
+from collections import OrderedDict
 
 
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        pass
+        count_overlaps = 0
+
+        intervals.sort(key=lambda interval: interval[0])
+
+        prev_interval = intervals[0]
+        for interval in intervals[1:]:
+            # overlapping case
+            if prev_interval[1] > interval[0]:
+                count_overlaps += 1
+
+                #keep the one which finishes first so that there is less chance of overlapping with future intervals
+                if prev_interval[1] >= interval[1]:
+                    # essentially change pre interval
+                    prev_interval = interval
+                # in the else case we continue to retain prev_interval as that finished earlier
+            else:
+                prev_interval = interval
+
+        return count_overlaps
+
+
+if __name__ == '__main__':
+    s = Solution()
+    intervals = s.eraseOverlapIntervals([[1, 100], [11, 22], [1, 11], [2, 12]])
+    assert intervals == 2, f"expected 2 was {intervals} instead"  # 2
+
+    overlap_intervals = s.eraseOverlapIntervals([[1, 2], [2, 3], [3, 4], [1, 3]])
+    assert overlap_intervals == 1, f"expected 1 was {overlap_intervals} instead"  # 1
+
+    erase_overlap_intervals = s.eraseOverlapIntervals([[1, 2], [1, 2], [1, 2]])
+    assert erase_overlap_intervals == 2, f"expected 2 was {erase_overlap_intervals} instead"  # 2
+
+    i = s.eraseOverlapIntervals([[1, 2], [2, 4], [1, 4]])
+    assert i == 1, f"expected 1 was {i} instead"
+
+    intervals1 = s.eraseOverlapIntervals([[1, 2], [2, 4]])
+    assert intervals1 == 0, f"expected 0 was {intervals1} instead"
