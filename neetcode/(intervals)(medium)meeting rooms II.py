94446802da7408bfd4bf8fe1c04c7fe7b0b2d36b
Parashara <parasharar@gmail.com>
2025-04-16 20:30:44 +0530

    meeting rooms II optimal solution

--- neetcode/(intervals)(medium)meeting rooms II.py
+++ neetcode/(intervals)(medium)meeting rooms II.py
@@ -83,6 +83,23 @@ class Solution:
 
         return days
 
+    # optimal way to do it, sort both by start and end times, and increment count when meeting is on and decrement count when a meeting is over
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+
+        res = count = 0
+        s = e = 0
+        while s < len(intervals):
+            if start[s] < end[e]:
+                s += 1
+                count += 1
+            else:
+                e += 1
+                count -= 1
+            res = max(res, count)
+        return res
+
 if __name__ == '__main__':
     s = Solution()
 
