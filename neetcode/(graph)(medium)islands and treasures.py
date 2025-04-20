6ca60b1c2d701ec576dbe781ab09a247f7a4c24c
Parashara <parasharar@gmail.com>
2025-04-20 13:32:14 +0530

    islands and treasures

--- neetcode/(graph)(medium)islands and treasures.py
+++ neetcode/(graph)(medium)islands and treasures.py
@@ -50,8 +50,108 @@ grid[i][j] is one of {-1, 0, 2147483647}
 
 '''
 from typing import List
+from collections import deque
 
 
 class Solution:
-    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
-        pass
+    # def wallsAndGates(self, grid: List[List[int]]) -> None: # Leetcode function
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None: # neetcode function
+        # magic constants
+        empty = (1 << 31) - 1
+        wall = -1
+        gate = 0
+        M = len(grid)
+        N = len(grid[0])
+        directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+
+        # helper function
+        is_inbounds = lambda i, j: 0 <= i < M and 0 <= j < N
+
+        # find the indices of all the gates
+        frontier = deque()
+        for i in range(M):
+            for j in range(N):
+                if grid[i][j] == gate:
+                    frontier.append((i, j, 0))
+
+        # multisource bfs
+        while frontier:
+            i, j, step = frontier.popleft()
+
+            # all base cases
+            if not is_inbounds(i, j):
+                continue
+
+            if grid[i][j] == wall:
+                continue
+
+            if grid[i][j] == gate:
+                for di, dj in directions:
+                    ni, nj = i + di, j + dj
+
+                    if (
+                            (0 <= ni < M and 0 <= nj < N) and
+                            (grid[ni][nj] != gate) and
+                            (grid[ni][nj] != wall) and
+                            (step + 1 <= grid[ni][nj])
+                    ):
+                        frontier.append((ni, nj, step + 1))
+
+                continue
+
+            # update only if the current step is the lowest for that cell
+            if step <= grid[i][j]:
+                grid[i][j] = step
+
+                for di, dj in directions:
+                    ni, nj = i + di, j + dj
+
+                    if (
+                            (0 <= ni < M and 0 <= nj < N) and
+                            (grid[ni][nj] != gate) and
+                            (grid[ni][nj] != wall) and
+                            (step + 1 <= grid[ni][nj])
+                    ):
+                        frontier.append((ni, nj, step + 1))
+
+        # return
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    # 0
+    rooms = [
+        [2147483647, 2147483647, 2147483647],
+        [2147483647, -1, 2147483647],
+        [0, 2147483647, 2147483647]
+    ]
+    expected = [
+        [2, 3, 4],
+        [1, -1, 3],
+        [0, 1, 2]
+    ]
+    s.islandsAndTreasure(rooms)
+    assert rooms == expected, f"res was {rooms}, expected {expected}"
+
+    # 1
+    rooms = [[-1]]
+    expected = [[-1]]
+    s.islandsAndTreasure(rooms)
+    assert rooms == expected, f"res was {rooms}, expected {expected}"
+
+    # 2
+    rooms = [
+        [2147483647, -1, 0, 2147483647],
+        [2147483647, 2147483647, 2147483647, -1],
+        [2147483647, -1, 2147483647, -1],
+        [0, -1, 2147483647, 2147483647]
+    ]
+    expected = [
+        [3, -1, 0, 1],
+        [2, 2, 1, -1],
+        [1, -1, 2, -1],
+        [0, -1, 3, 4]
+    ]
+    s.islandsAndTreasure(rooms)
+    assert rooms == expected, f"res was {rooms}, expected {expected}"
