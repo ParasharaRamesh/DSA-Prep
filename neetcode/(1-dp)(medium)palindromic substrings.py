45eed5cc1dec0fc02b7964953e230bf08f1fc54c
Parashara <parasharar@gmail.com>
2025-04-28 19:29:18 +0530

    palindromic substrings

--- neetcode/(1-dp)(medium)palindromic substrings.py
+++ neetcode/(1-dp)(medium)palindromic substrings.py
@@ -25,5 +25,20 @@ s consists of lowercase English letters.
 
 
 class Solution:
+    #with center at one of the indexes, do two pointer and keep counting
     def countSubstrings(self, s: str) -> int:
-        pass
+        count = 0
+        for i in range(len(s)):
+            odd_count = self.count_palindromes(i,i,s)
+            even_count = self.count_palindromes(i,i+1,s)
+            count += odd_count + even_count
+        return count
+
+    def count_palindromes(self, l, r, s):
+        m = len(s)
+        count = 0
+        while l >= 0 and r < m and s[l] == s[r]:
+            count += 1
+            l -= 1
+            r += 1
+        return count
