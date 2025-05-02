a9765184fd82ede71d8007b422f13789114358ac
Parashara <parasharar@gmail.com>
2025-05-02 20:40:29 +0530

    target sum

--- neetcode/(2-dp)(medium)target sum.py
+++ neetcode/(2-dp)(medium)target sum.py
@@ -25,9 +25,63 @@ Constraints:
 -1000 <= target <= 1000
 
 '''
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
+    def findTargetSumWays_memo(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        cache = dict()
+
+        # topdown recursion
+        def helper(i, left):
+            key = (i, left)
+
+            if key in cache:
+                return cache[key]
+
+            if i == n:
+                cache[key] = 1 if left == 0 else 0
+                return cache[key]
+
+            cache[key] = helper(i + 1, left - nums[i]) + helper(i + 1, left + nums[i])
+            return cache[key]
+
+        return helper(0, target)
+
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        pass
+        n = len(nums)
+        cache = defaultdict(int)
+
+        # case2: if i == n and left == 0 -> 1 | All other cases are 0 by defaultdict
+        cache[(n, 0)] = 1
+
+        total_target = sum(nums)
+        for i in range(n - 1, -1, -1):
+            for left in range(-total_target, total_target + 1):
+                key = (i, left)
+                cache[key] = cache[(i + 1, left - nums[i])] + cache[(i + 1, left + nums[i])]
+
+        return cache[(0, target)]
+
+if __name__ == '__main__':
+    s = Solution()
+
+    nums = [2, 2]
+    target = 0
+    expected = 2
+    ans = s.findTargetSumWays(nums, target)
+    assert expected == ans, f"{expected = } {ans = }"
+
+    nums = [2, 2, 2]
+    target = 4
+    expected = 0
+    ans = s.findTargetSumWays(nums, target)
+    assert expected == ans, f"{expected = } {ans = }"
+
+    nums = [2, 2, 2]
+    target = 2
+    expected = 3
+    ans = s.findTargetSumWays(nums, target)
+    assert expected == ans, f"{expected = } {ans = }"
