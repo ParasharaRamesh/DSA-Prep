e44e69445014ec6fdba711cf24287dd73821066a
Parashara <parasharar@gmail.com>
2025-05-04 21:15:39 +0530

    num distinct subsequences

--- neetcode/(2-dp)(hard)distinct subsequences.py
+++ neetcode/(2-dp)(hard)distinct subsequences.py
@@ -35,5 +35,91 @@ s and t consist of English letters.
 
 
 class Solution:
-    def numDistinct(self, s: str, t: str) -> int:
-        pass
+    def numDistinct_memo(self, s: str, t: str) -> int:
+        # trivial cases
+        if len(s) < len(t):
+            return 0
+
+        if len(s) == len(t):
+            return 1 if s == t else 0
+
+        m = len(s)
+        n = len(t)
+
+        cache = dict()
+
+        def helper(i, j):
+            key = (i, j)
+
+            if key in cache:
+                return cache[key]
+
+            # all chars in t matched!
+            if j == n:
+                cache[key] = 1
+                return 1
+
+            # exhausted i
+            if i == m:
+                cache[key] = 0
+                return 0
+
+            total = 0
+
+            # include
+            if s[i] == t[j]:
+                total += helper(i + 1, j + 1)
+
+            # exclude
+            total += helper(i + 1, j)
+
+            cache[key] = total
+            return total
+
+        return helper(0, 0)
+
+    def numDistinct_tab(self, s: str, t: str) -> int:
+        # trivial cases
+        if len(s) < len(t):
+            return 0
+
+        if len(s) == len(t):
+            return 1 if s == t else 0
+
+        m = len(s)
+        n = len(t)
+
+        cache = dict()
+
+        # base cases ?
+        # exhausted i
+        # if i == m:
+        #     cache[key] = 0
+        #     return 0
+
+        for j in range(n):
+            cache[(m, j)] = 0
+
+        # all chars in t matched!
+        # if j == n:
+        #     cache[key] = 1
+        #     return 1
+        for i in range(m + 1):
+            cache[(i, n)] = 1
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                key = (i, j)
+
+                total = 0
+
+                # include
+                if s[i] == t[j]:
+                    total += cache[(i + 1, j + 1)]
+
+                # exclude
+                total += cache[(i + 1, j)]
+
+                cache[key] = total
+
+        return cache[(0, 0)]
