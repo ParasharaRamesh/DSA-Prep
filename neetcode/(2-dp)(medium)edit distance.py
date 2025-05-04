a5b07c3c98f3781cce644fde4a8a4a2155f13b19
Parashara <parasharar@gmail.com>
2025-05-04 22:41:16 +0530

    edit distance

--- neetcode/(2-dp)(medium)edit distance.py
+++ neetcode/(2-dp)(medium)edit distance.py
@@ -36,5 +36,117 @@ word1 and word2 consist of lowercase English letters.
 
 
 class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        pass
+    def minDistance_memo(self, word1: str, word2: str) -> int:
+        m = len(word1)
+        n = len(word2)
+
+        cache = dict()
+
+        def helper(i, j):
+            key = (i, j)
+
+            if key in cache:
+                return cache[key]
+
+            # base cases
+            if i == m and j < n:
+                # insert
+                cache[key] = n - j
+                return n - j
+
+            if i < m and j == n:
+                # delete
+                cache[key] = m - i
+                return m - i
+
+            if i == m and j == n:
+                cache[key] = 0
+                return 0
+
+            # actual logic
+            if word1[i] == word2[j]:
+                cache[key] = helper(i + 1, j + 1)
+                return cache[key]
+
+            # try everything
+            subs = 1 + helper(i + 1, j + 1)
+            ins = 1 + helper(i, j + 1)
+            rem = 1 + helper(i + 1, j)
+
+            cache[key] = min(subs, ins, rem)
+            return cache[key]
+
+        return helper(0, 0)
+
+    def minDistance_tab(self, word1: str, word2: str) -> int:
+        m = len(word1)
+        n = len(word2)
+
+        cache = dict()
+
+        cache[(m, n)] = 0
+
+        for i in range(m):
+            cache[(i , n)] = m - i
+
+        for j in range(n):
+            cache[(m, j)] = n - j
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                key = (i, j)
+
+                # actual logic
+                if word1[i] == word2[j]:
+                    cache[key] = cache[(i + 1, j + 1)]
+                    continue
+
+                # try everything
+                subs = 1 + cache[(i + 1, j + 1)]
+                ins = 1 + cache[(i, j + 1)]
+                rem = 1 + cache[(i + 1, j)]
+
+                cache[key] = min(subs, ins, rem)
+
+        return cache[(0, 0)]
+
+if __name__ == '__main__':
+    s = Solution()
+
+    word1 = "a"
+    word2 = "ab"
+    expected = 1
+    ans = s.minDistance(word1, word2)
+    assert expected == ans, f"{expected = }, {ans = }"
+
+    word1 = "ab"
+    word2 = "a"
+    expected = 1
+    ans = s.minDistance(word1, word2)
+    assert expected == ans, f"{expected = }, {ans = }"
+
+    word1 = "a"
+    word2 = "b"
+    expected = 1
+    ans = s.minDistance(word1, word2)
+    assert expected == ans, f"{expected = }, {ans = }"
+
+    word1 = "ab"
+    word2 = "ab"
+    expected = 0
+    ans = s.minDistance(word1, word2)
+    assert expected == ans, f"{expected = }, {ans = }"
+
+    word1 = "neatcdee"
+    word2 = "neetcode"
+    expected = 3
+    ans = s.minDistance(word1, word2)
+    assert expected == ans, f"{expected = }, {ans = }"
+
+    word1 = "monkeys"
+    word2 = "money"
+    expected = 2
+    ans = s.minDistance(word1, word2)
+    assert expected == ans, f"{expected = }, {ans = }"
+
+
