f3645a1ce8b00b43d047bf7d29b376e4272c0b95
Parashara <parasharar@gmail.com>
2025-05-08 19:11:10 +0530

    jump game II

--- neetcode/(greedy)(medium)jump game II.py
+++ neetcode/(greedy)(medium)jump game II.py
@@ -30,15 +30,49 @@ from typing import List
 class Solution:
     def jump(self, nums: List[int]) -> int:
         l, r = 0, 0
-        res = 0
+        jumps = 0
 
+        # kind of like sliding window
         while r < len(nums) - 1:
+            # inside the range from [l,r] figure out what is the furthest you can reach. Therefore in one jump you can reach anywhere from [l, farthest]
             farthest = 0
             for i in range(l, r + 1):
                 farthest = max(farthest, i + nums[i])
+
+            # once we find the max farthest in the range [l, r] that constitutes as one jump
+            jumps += 1
+
+            # move l to the next one & move r to the current farthest point
             l = r + 1
             r = farthest
-            res += 1
 
-        return res
+        # the moment r is greater than the last index, we just stop
+        return jumps
 
+if __name__ == '__main__':
+    s = Solution()
+
+    nums = [7,0,9,6,9,6,1,7,9,0,1,2,9,0,3]
+    expected = 2
+    ans = s.jump(nums)
+    assert expected == ans, f"{expected = }, {ans = }"
+
+    nums = [2]
+    expected = 0
+    ans = s.jump(nums)
+    assert expected == ans, f"{expected = }, {ans = }"
+
+    nums = [1, 2]
+    expected = 1
+    ans = s.jump(nums)
+    assert expected == ans, f"{expected = }, {ans = }"
+
+    nums = [2,1,2,1,0]
+    expected = 2
+    ans = s.jump(nums)
+    assert expected == ans, f"{expected = }, {ans = }"
+
+    nums = [2,4,1,1,1]
+    expected = 2
+    ans = s.jump(nums)
+    assert expected == ans, f"{expected = }, {ans = }"
