c541fe5ccc8e719f599dbfe60d0ad482d88daa09
Parashara <parasharar@gmail.com>
2025-05-10 00:22:50 +0530

    merge triplets to form target triplet

--- neetcode/(greedy)(medium)merge triplets to form target triplet.py
+++ neetcode/(greedy)(medium)merge triplets to form target triplet.py
@@ -30,8 +30,103 @@ Constraints:
 
 from typing import List
 
-
+'''
+. trivial case 1: if target[i] not found in any triplet[j][i] -> return false
+. trivial case 2: as we iterate in case we find the triplet which exactly same as target then return and finish
+. keep temp
+. if not, for each triplet :
+    - check temp == triplet
+    - choose to not use it at all:
+        . if each is > target[i] -> can never contribute when taken max (other one will spoil the party)
+    - choose to use it:
+        . in case anything matches and others are lesser than equal to target
+        . keep doing the max operation with the current best and check if that is equal to target whenever there is some match
+'''
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        pass
 
+        # trvivial cases
+        a_present = False
+        b_present = False
+        c_present = False
+
+        for triplet in triplets:
+            a, b, c = triplet
+
+            # exact match
+            if triplet == target:
+                return True
+
+            # partial matches
+            if a == target[0]:
+                a_present = True
+
+            if b == target[1]:
+                b_present = True
+
+            if c == target[2]:
+                c_present = True
+
+        # trvial case no 2:
+        if not (a_present and b_present and c_present):
+            return False
+
+        best = None
+        tx, ty, tz = target
+
+        for triplet in triplets:
+            x, y, z = triplet
+
+            # ignore it
+            if x > tx or y > ty or z > tz:
+                continue
+
+            # choose it
+            x_equal = x == tx
+            y_equal = y == ty
+            z_equal = z == tz
+            atleast_one = x_equal or y_equal or z_equal
+            all_le = (x <= tx) and (y <= ty) and (z <= tz)
+
+            if atleast_one and all_le:
+                best = [max(best[0], triplet[0]), max(best[1], triplet[1]), max(best[2], triplet[2])] if best else triplet
+
+                if best == target:
+                    return True
+
+        # default
+        return False
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    triplets = [[2, 5, 3], [1, 8, 4], [1, 7, 5]]
+    target = [2, 7, 5]
+    expected = True
+    res = s.mergeTriplets(triplets, target)
+    assert expected == res, f"{expected = }, {res = }"
+
+    triplets = [[1, 2, 3], [7, 1, 1]]
+    target = [7, 2, 3]
+    expected = True
+    res = s.mergeTriplets(triplets, target)
+    assert expected == res, f"{expected = }, {res = }"
+
+    triplets = [[2, 5, 3], [2, 3, 4], [1, 2, 5], [5, 2, 3]]
+    target = [5, 5, 5]
+    expected = True
+    res = s.mergeTriplets(triplets, target)
+    assert expected == res, f"{expected = }, {res = }"
+
+    triplets = [[2, 5, 6], [1, 4, 4], [5, 7, 5]]
+    target = [5, 4, 6]
+    expected = False
+    res = s.mergeTriplets(triplets, target)
+    assert expected == res, f"{expected = }, {res = }"
+
+    triplets = [[3, 4, 5], [4, 5, 6]]
+    target = [3, 2, 5]
+    expected = False
+    res = s.mergeTriplets(triplets, target)
+    assert expected == res, f"{expected = }, {res = }"
