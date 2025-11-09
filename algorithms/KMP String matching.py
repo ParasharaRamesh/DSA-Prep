392fdadfd4c6bc978ca8283d2cb3675f211da5dd
Parashara <parasharar@gmail.com>
2025-11-09 17:44:45 +0530

    minor change to kmp string matching algorithm

--- algorithms/KMP String matching.py
+++ algorithms/KMP String matching.py
@@ -1,3 +1,4 @@
+#BRUTE FORCE AND INEFFICIENT
 def calculateLengthOfLongestPrefixCumSuffix(word):
     n = len(word)
     i = 0
@@ -23,6 +24,35 @@ def computePrefix(substring):
         prefix[i] = calculateLengthOfLongestPrefixCumSuffix(substring[:i+1])
 
     return prefix
+
+# THIS Is apparently the better approach
+def compute_kmp_prefix(substring):
+    m = len(substring)
+    lps = [0] * m  # Longest Proper Prefix which is also a Suffix
+
+    length = 0  # length of the previous longest prefix suffix
+    i = 1  # index for the substring ( start at 1 because lps[0] is anyways 0!)
+
+    while i < m:
+        if substring[i] == substring[length]:
+            # Case 1: Match found. Extend the previous LPS length.
+            length += 1
+            lps[i] = length
+            i += 1
+        else:
+            # Case 2: Mismatch.
+            if length != 0:
+                # Fall back! Reset 'length' to the LPS length of the previous character (i-1)
+                # The value lps[length - 1] is the next shortest border we can try.
+                length = lps[length - 1]
+                # NOTE: We do NOT increment 'i' here, we re-check with the new 'length' in the next iteration.
+            else:
+                # Case 3: length is 0 (no prefix/suffix match found).
+                # Set LPS for the current character to 0 and move to the next character.
+                lps[i] = 0
+                i += 1
+
+    return lps
 
 def knuthMorrisPrattAlgorithm(string, substring):
     prefix = computePrefix(substring)
