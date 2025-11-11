53aab871f28450221c5ef82bfdfb0c3f0188ef8f
Parashara <parasharar@gmail.com>
2025-11-11 21:47:50 +0530

    updated the dfs sample code

--- algorithms/dfs.py
+++ algorithms/dfs.py
@@ -1,17 +1,69 @@
-class Node:
-    def __init__(self, name):
-        self.children = []
-        self.name = name
+from collections import deque
+from typing import Dict, Iterable, List, Hashable
 
-    def addChild(self, name):
-        self.children.append(Node(name))
-        return self
 
-    def depthFirstSearch(self, array):
-        dfs = [self]
-        while len(dfs) != 0:
-            node = dfs.pop()
-            array.append(node.name)
-            for child in node.children[::-1]:
-                dfs.append(child)
-        return array
+def dfs_recursive(graph: Dict[Hashable, Iterable[Hashable]], start: Hashable) -> List[Hashable]:
+    """
+    Recursive Depth-First Search.
+
+    graph: adjacency list, e.g. {'A': ['B', 'C'], 'B': ['D'], 'C': [], 'D': []}
+    start: starting node/key present in the graph
+
+    returns: list of nodes in visitation order
+    """
+    order: List[Hashable] = []
+    visited = set()
+
+    def visit(node: Hashable) -> None:
+        if node in visited:
+            return
+        visited.add(node)
+        order.append(node)
+        for neighbor in graph.get(node, []):
+            visit(neighbor)
+
+    visit(start)
+    return order
+
+
+def dfs_iterative(graph: Dict[Hashable, Iterable[Hashable]], start: Hashable) -> List[Hashable]:
+    """
+    Iterative Depth-First Search using an explicit stack.
+
+    Push neighbors in reverse order to mimic recursive pre-order on lists.
+    """
+    order: List[Hashable] = []
+    visited = set()
+    stack: List[Hashable] = [start]
+
+    while stack:
+        node = stack.pop()
+        if node in visited:
+            continue
+        visited.add(node)
+        order.append(node)
+        neighbors = list(graph.get(node, []))
+        for neighbor in reversed(neighbors):
+            if neighbor not in visited:
+                stack.append(neighbor)
+
+    return order
+
+
+def bfs(graph: Dict[Hashable, Iterable[Hashable]], start: Hashable) -> List[Hashable]:
+    """
+    Breadth-First Search using a queue.
+    """
+    order: List[Hashable] = []
+    visited = set([start])
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        order.append(node)
+        for neighbor in graph.get(node, []):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+    return order
