3f9c548c5c34d99dc6f1415d3c4b53b7f7a57be8
Parashara <parasharar@gmail.com>
2025-11-16 21:03:30 +0530

    added longest common prefix

--- neetcode/(array)(easy)contains duplicate.py
+++ neetcode/(array)(easy)contains duplicate.py
@@ -9,10 +9,21 @@ from typing import List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # option 1 (check if length of set is less than length of array)
         return len(set(nums)) < len(nums)
+
+        # option 2 (use counter to count the number of times each number appears)
         # counts = Counter(nums)
         # for num in counts:
         #     count = counts[num]
         #     if count > 1:
         #         return True
+        # return False
+
+        # option 3 (use hashset to see ifthere is a duplicate anywhere)
+        # seen = set()
+        # for num in nums:
+        #     if num in seen:
+        #         return True
+        #     seen.add(num)
         # return False
---
+++ neetcode/(array)(easy)longest common prefix.py
@@ -0,0 +1,42 @@
+'''
+You are given an array of strings strs. Return the longest common prefix of all the strings.
+
+If there is no longest common prefix, return an empty string "".
+
+Example 1:
+
+Input: strs = ["bat","bag","bank","band"]
+
+Output: "ba"
+Example 2:
+
+Input: strs = ["dance","dag","danger","damage"]
+
+Output: "da"
+Example 3:
+
+Input: strs = ["neet","feet"]
+
+Output: ""
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] is made up of lowercase English letters if it is non-empty.
+'''
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 1:
+            return strs[0]
+
+        res = ""
+        all_chars = zip(*strs)
+
+        for t in all_chars:
+            if len(set(t)) == 1:
+                res += t[0]
+            else:
+                break
+
+        return res
