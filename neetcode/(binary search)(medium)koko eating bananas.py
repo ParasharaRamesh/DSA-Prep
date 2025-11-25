fb51c97852abdce7efd450c0da41dba6501d24d2
Parashara <parasharar@gmail.com>
2025-11-25 19:43:33 +0530

    add binary search solution for Koko Eating Bananas problem.

--- neetcode/(binary search)(medium)koko eating bananas.py
+++ neetcode/(binary search)(medium)koko eating bananas.py
@@ -39,7 +39,7 @@ class Solution:
 
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         l = 1
-        r = sum(piles)
+        r = max(piles)
 
         while l <= r:
             k = l + (r - l) // 2
