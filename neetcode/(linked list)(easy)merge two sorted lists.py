f5a97330aac3400eeb5155055aa73c4dbca775da
Parashara <parasharar@gmail.com>
2025-11-26 18:38:44 +0530

    recursive soln + inplace iterative solution for merge two sorted linked lists

--- neetcode/(linked list)(easy)merge two sorted lists.py
+++ neetcode/(linked list)(easy)merge two sorted lists.py
@@ -8,6 +8,7 @@ class ListNode:
 
 
 class Solution:
+    # old solution - iterative
     def addValToTail(self, head, tail, val):
         if tail:
             tail.next = ListNode(val)
@@ -44,3 +45,46 @@ class Solution:
             return h2
         else:
             return None
+
+    # recursive solution
+    def mergeTwoLists_recursive(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
+
+    # better iterative solution in place
+    def mergeTwoLists_iterative(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        res_curr = ListNode()
+        res_head = res_curr
+
+        curr1 = list1
+        curr2 = list2
+
+        while curr1 and curr2:
+            after1 = curr1.next
+            after2 = curr2.next
+
+            if curr1.val <= curr2.val:
+                res_curr.next = curr1
+                curr1.next = None #disconnect
+                curr1 = after1
+            else:
+                res_curr.next = curr2
+                curr2.next = None
+                curr2 = after2
+
+            res_curr = res_curr.next
+
+        if curr1: 
+            res_curr.next = curr1
+        elif curr2: 
+            res_curr.next = curr2
+
+        return res_head.next
