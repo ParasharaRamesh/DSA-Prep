66d41c9815a58ad3cd3aaea72ef71b702accaf19
Parashara <parasharar@gmail.com>
2025-12-02 17:26:35 +0530

    Adding neetcode's solution for n queens as it is more easier to read and more elegant

--- neetcode/(backtracking)(hard)n queens.py
+++ neetcode/(backtracking)(hard)n queens.py
@@ -28,6 +28,7 @@ from collections import defaultdict
 from copy import deepcopy
 
 class Solution:
+    # my old approach very ugly
     def solveNQueens(self, n: int) -> List[List[str]]:
         res = set()
         board = [["."] * n for i in range(n)]
@@ -115,6 +116,42 @@ class Solution:
 
         return res
 
+class NeetcodeSolution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        col = set()
+        posDiag = set()
+        negDiag = set()
+
+        res = []
+        board = [["."] * n for i in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+
+                backtrack(r + 1)
+
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+
+        backtrack(0)
+        return res
+
 if __name__ == '__main__':
     s = Solution()
+    n = NeetcodeSolution()
     print(s.solveNQueens(4))
+    print(n.solveNQueens(4))
